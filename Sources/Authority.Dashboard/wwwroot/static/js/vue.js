aa22f8fc0341c48bfe558296a281196ea3c53eb4
dansun <daniel@authority.se>
2018-09-18 19:16:48 +0200

    fixed måsvinge

--- Sources/Authority.Dashboard/wwwroot/static/js/vue.js
+++ Sources/Authority.Dashboard/wwwroot/static/js/vue.js
@@ -1,7 +1,7 @@
 Vue.component('module', {
-  props: ['module'],
-  template: `
-    <div class="module" v-bind:data-module-id="module.id" style="background-color:' + item.color + ';height:' + item.height + 'px;width:' + item.width + 'px;">
+    props: ['module'],
+    template: `
+    <div class="module" v-bind:data-module-id="module.id">
       <h4>{{ module.id }}</h4>
       <p>{{ module.value }}</p>
     </div>
@@ -9,35 +9,9 @@ Vue.component('module', {
 });
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    modules: []
-  },
-  created: function() {
-    let vm = this;
-    fetch('/api/module')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        data.forEach(item => {
-          item.value = '';
-        });
-        vm.modules = data;
-        this.initModules(vm.modules);
-      });
-  },
-  methods: {
-    initModules: function(modules) {
-      let timers = [];
-      for (let i = 0; i < modules.length; i++) {
-        var module = modules[i];
-        timers.push(
-          setInterval(() => {
-            this.handleModuleRequest(module, i);
-          }, module.reloadInterval * 1000)
-        );
-      }
+    el: '#app',
+    data: {
+        modules: []
     },
     created: function () {
         let vm = this;
@@ -46,7 +20,7 @@ var app = new Vue({
                 return response.json();
             })
             .then(data => {
-                data.fetchModules.forEach(item => {
+                data.forEach(item => {
                     item.value = '';
                 });
                 vm.modules = data;
@@ -61,18 +35,47 @@ var app = new Vue({
                 timers.push(
                     setInterval(() => {
                         this.handleModuleRequest(module, i);
-                    }, module.reloadInterval * 1000)
+                    },
+                        module.reloadInterval * 1000)
                 );
             }
         },
-        handleModuleRequest: function (module, index) {
-            fetch(module.url)
+        created: function () {
+            let vm = this;
+            fetch('/api/module')
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
-                    this.modules[0].value = data;
+                    data.fetchModules.forEach(item => {
+                        item.value = '';
+                    });
+                    vm.modules = data;
+                    this.initModules(vm.modules);
                 });
+        },
+        methods: {
+            initModules: function (modules) {
+                let timers = [];
+                for (let i = 0; i < modules.length; i++) {
+                    var module = modules[i];
+                    timers.push(
+                        setInterval(() => {
+                            this.handleModuleRequest(module, i);
+                        },
+                            module.reloadInterval * 1000)
+                    );
+                }
+            },
+            handleModuleRequest: function (module, index) {
+                fetch(module.url)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        this.modules[0].value = data;
+                    });
+            }
         }
     }
 });
